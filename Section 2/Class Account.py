89862eb61f473bf9ba68f60df64c54a549d4ea9c
stormxsam <stormxsam@gmail.com>
2022-02-23 06:08:50 +0800

    update

---
+++ Section 2/Class Account.py
@@ -0,0 +1,12 @@
+class Account:
+    def __init__(self, name, balance):
+        self.id
+        self.name
+        self.balance
+
+    def deposit(self, amount):
+
+    def withdraw(self, amount):
+
+    def __str__(self):
+        return f"{self.name}'s account. Balance: {self.balance}"
